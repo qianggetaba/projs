62202397ce853f8846b7eae6340aa41f7c0d3f1c
qianggetaba <hui740024906@163.com>
2019-12-10 19:30:41 +0800

    aop

--- scatter/spring.java
+++ scatter/spring.java
@@ -1,4 +1,8 @@
-判断bean是否是proxy，来自EurekaClientAutoConfiguration#eurekaClient()
+判断bean是否被aop proxy，获取直接对象，来自EurekaClientAutoConfiguration#eurekaClient()
 if (AopUtils.isAopProxy(manager)) {
 				appManager = ProxyUtils.getTargetObject(manager);
 			}
+
+当service被aop切面，但是被调用函数里调用当前类的其他方法没有被aop，把this.callMethodB()改为下面的调用方法
+((Service) AopContext.currentProxy()).callMethodB();
+
